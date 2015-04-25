2fb81dfa46695a90478185763318021d273cfc11
dangerworm <dangerworm@gmail.com>
2015-04-25 03:36:01 +0100

    Initial Commit

---
+++ MouseLab/MouseLab.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MouseLab
+{
+    partial class frmMouseLab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.stsStrip = new System.Windows.Forms.StatusStrip();
+            this.tslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tmrTimer = new System.Windows.Forms.Timer(this.components);
+            this.lstData = new System.Windows.Forms.ListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.imgGraph = new System.Windows.Forms.PictureBox();
+            this.rdoButtons = new System.Windows.Forms.RadioButton();
+            this.rdoYAxis = new System.Windows.Forms.RadioButton();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.stsStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stsStrip
+            // 
+            this.stsStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslStatus});
+            this.stsStrip.Location = new System.Drawing.Point(0, 354);
+            this.stsStrip.Name = "stsStrip";
+            this.stsStrip.Size = new System.Drawing.Size(784, 22);
+            this.stsStrip.TabIndex = 4;
+            this.stsStrip.Text = "statusStrip1";
+            // 
+            // tslStatus
+            // 
+            this.tslStatus.Name = "tslStatus";
+            this.tslStatus.Size = new System.Drawing.Size(39, 17);
+            this.tslStatus.Text = "Ready";
+            // 
+            // tmrTimer
+            // 
+            this.tmrTimer.Tick += new System.EventHandler(this.tmrTimer_Tick);
+            // 
+            // lstData
+            // 
+            this.lstData.FormattingEnabled = true;
+            this.lstData.Location = new System.Drawing.Point(618, 12);
+            this.lstData.Name = "lstData";
+            this.lstData.Size = new System.Drawing.Size(154, 303);
+            this.lstData.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(618, 323);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(154, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save Data";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // imgGraph
+            // 
+            this.imgGraph.BackColor = System.Drawing.Color.Black;
+            this.imgGraph.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imgGraph.Location = new System.Drawing.Point(12, 12);
+            this.imgGraph.Name = "imgGraph";
+            this.imgGraph.Size = new System.Drawing.Size(600, 280);
+            this.imgGraph.TabIndex = 1;
+            this.imgGraph.TabStop = false;
+            this.imgGraph.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imgGraph_MouseDown);
+            this.imgGraph.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imgGraph_MouseMove);
+            this.imgGraph.MouseUp += new System.Windows.Forms.MouseEventHandler(this.imgGraph_MouseUp);
+            // 
+            // rdoButtons
+            // 
+            this.rdoButtons.AutoSize = true;
+            this.rdoButtons.Checked = true;
+            this.rdoButtons.Location = new System.Drawing.Point(12, 298);
+            this.rdoButtons.Name = "rdoButtons";
+            this.rdoButtons.Size = new System.Drawing.Size(61, 17);
+            this.rdoButtons.TabIndex = 0;
+            this.rdoButtons.TabStop = true;
+            this.rdoButtons.Text = "Buttons";
+            this.rdoButtons.UseVisualStyleBackColor = true;
+            this.rdoButtons.CheckedChanged += new System.EventHandler(this.rdoButtons_CheckedChanged);
+            // 
+            // rdoYAxis
+            // 
+            this.rdoYAxis.AutoSize = true;
+            this.rdoYAxis.Location = new System.Drawing.Point(12, 321);
+            this.rdoYAxis.Name = "rdoYAxis";
+            this.rdoYAxis.Size = new System.Drawing.Size(53, 17);
+            this.rdoYAxis.TabIndex = 1;
+            this.rdoYAxis.Text = "Y-axis";
+            this.rdoYAxis.UseVisualStyleBackColor = true;
+            this.rdoYAxis.CheckedChanged += new System.EventHandler(this.rdoYAxis_CheckedChanged);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(279, 298);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(65, 48);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Start Recording";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // frmMouseLab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 376);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.rdoYAxis);
+            this.Controls.Add(this.rdoButtons);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lstData);
+            this.Controls.Add(this.stsStrip);
+            this.Controls.Add(this.imgGraph);
+            this.MaximizeBox = false;
+            this.Name = "frmMouseLab";
+            this.Text = "MouseLab";
+            this.Load += new System.EventHandler(this.frmMouseLab_Load);
+            this.Move += new System.EventHandler(this.frmMouseLab_Move);
+            this.Resize += new System.EventHandler(this.frmMouseLab_Resize);
+            this.stsStrip.ResumeLayout(false);
+            this.stsStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip stsStrip;
+        private System.Windows.Forms.ToolStripStatusLabel tslStatus;
+        private System.Windows.Forms.Timer tmrTimer;
+        private System.Windows.Forms.ListBox lstData;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+        private System.Windows.Forms.PictureBox imgGraph;
+        private System.Windows.Forms.RadioButton rdoButtons;
+        private System.Windows.Forms.RadioButton rdoYAxis;
+        private System.Windows.Forms.Button btnGo;
+
+    }
+}
+
---
+++ MouseLab/MouseLab.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MouseLab
+{
+    public partial class frmMouseLab : Form
+    {
+        const int LEFTDOWN = 0;
+        const int LEFTUP = 1;
+        const int MIDDOWN = 2;
+        const int MIDUP = 3;
+        const int RIGHTDOWN = 4;
+        const int RIGHTUP = 5;
+        const int ALLUP = 7;
+
+        Bitmap bGraph;
+        Graphics gGraph;
+
+        DateTime startTime;
+        DateTime stopTime;
+
+        Dictionary<int, string> buttons;
+        Dictionary<double, int> data;
+
+        Point loggingPosition;
+        bool logButtons;
+        bool running;
+        int maxY;
+        int minY;
+        int mousePos;
+
+        public frmMouseLab()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMouseLab_Load(object sender, EventArgs e)
+        {
+            logButtons = true;
+            running = false;
+
+            bGraph = new Bitmap(imgGraph.Width, imgGraph.Height);
+            gGraph = Graphics.FromImage(bGraph);
+            imgGraph.Image = bGraph;
+
+            loggingPosition = new Point(Left + imgGraph.Left + imgGraph.Width / 2,
+                                        Top + imgGraph.Top + imgGraph.Height);
+
+            buttons = new Dictionary<int, string>();
+            buttons.Add(LEFTDOWN, "Left Down");
+            buttons.Add(LEFTUP, "Left Up");
+            buttons.Add(MIDDOWN, "Middle Down");
+            buttons.Add(MIDUP, "Middle Up");
+            buttons.Add(RIGHTDOWN, "Right Down");
+            buttons.Add(RIGHTUP, "Right Up");
+            buttons.Add(ALLUP, "All Up");
+
+            data = new Dictionary<double, int>();
+
+            maxY = 0;
+            minY = 0;
+            mousePos = 0;
+        }
+
+        private void frmMouseLab_Resize(object sender, EventArgs e)
+        {
+            bGraph = new Bitmap(imgGraph.Width, imgGraph.Height);
+            gGraph = Graphics.FromImage(bGraph);
+        }
+
+        private void UpdateStatus()
+        {
+            if (running)
+            {
+                tslStatus.Text = "Running";
+
+                if (!logButtons)
+                {
+                    tslStatus.Text += ": Click to stop";
+                }
+            }
+            else
+            {
+                double runTime = (stopTime - startTime).TotalSeconds;
+                if (runTime == 0)
+                {
+                    tslStatus.Text = "Ready";
+                }
+                else
+                {
+                    tslStatus.Text = "Ran for " + (stopTime - startTime).TotalSeconds + " seconds: Ready";
+                }
+            }
+        }
+
+        private void StartRecording()
+        {
+            mousePos = 0;
+            maxY = 0;
+            minY = 0;
+            startTime = DateTime.Now;
+            data.Clear();
+            lstData.Items.Clear();
+            tmrTimer.Enabled = true;
+            running = true;
+            Cursor.Position = loggingPosition;
+            btnGo.Text = "Stop Recording";
+            UpdateStatus();
+        }
+
+        private void StopRecording()
+        {
+            stopTime = DateTime.Now;
+            tmrTimer.Enabled = false;
+            running = false;
+            btnGo.Text = "Start Recording";
+            UpdateStatus();
+        }
+
+        private void StoreClick(MouseEventArgs e, bool isDown)
+        {
+            if (logButtons)
+            {
+                double time = (DateTime.Now - startTime).TotalSeconds;
+                string button = "";
+
+                try
+                {
+                    switch (e.Button)
+                    {
+                        case System.Windows.Forms.MouseButtons.Left:
+                            if (isDown)
+                            {
+                                data.Add(time, LEFTDOWN);
+                                button = "Left Down";
+                            }
+                            else
+                            {
+                                data.Add(time, LEFTUP);
+                                button = "Left Up";
+                            }
+                            break;
+                        case System.Windows.Forms.MouseButtons.Middle:
+                            if (isDown)
+                            {
+                                data.Add(time, MIDDOWN);
+                                button = "Middle Down";
+                            }
+                            else
+                            {
+                                data.Add(time, MIDUP);
+                                button = "Middle Up";
+                            }
+                            break;
+                        case System.Windows.Forms.MouseButtons.Right:
+                            if (isDown)
+                            {
+                                data.Add(time, RIGHTDOWN);
+                                button = "Right Down";
+                            }
+                            else
+                            {
+                                data.Add(time, RIGHTUP);
+                                button = "Right Up";
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (ArgumentException)
+                {
+                    try
+                    {
+                        data.Add(time, ALLUP);
+                    }
+                    catch (ArgumentException)
+                    { }
+                }
+
+                lstData.Items.Add(time + "s: " + button);
+            }
+            else
+            {
+                if (isDown)
+                {
+                    StopRecording();
+                }
+            }
+        }
+
+        private void StoreMove(MouseEventArgs e)
+        {
+            if (!logButtons && running && Cursor.Position != loggingPosition)
+            {
+                double time = (DateTime.Now - startTime).TotalSeconds;
+                mousePos += 248 - e.Y;
+                Cursor.Position = loggingPosition;
+
+                if (mousePos > maxY)
+                {
+                    maxY = mousePos;
+                }
+                else if (mousePos < minY)
+                {
+                    minY = mousePos;
+                }
+
+                try
+                {
+                    data.Add(time, mousePos);
+                }
+                catch (ArgumentException)
+                { }
+
+                lstData.Items.Add(time + "s: " + mousePos);
+            }
+        }
+
+        private void imgGraph_MouseDown(object sender, MouseEventArgs e)
+        {
+            StoreClick(e, true);
+        }
+
+        private void imgGraph_MouseMove(object sender, MouseEventArgs e)
+        {
+            StoreMove(e);
+        }
+
+        private void imgGraph_MouseUp(object sender, MouseEventArgs e)
+        {
+            StoreClick(e, false);
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            running = !running;
+
+            if (running)
+            {
+                StartRecording();
+            }
+            else
+            {
+                StopRecording();
+            }
+
+        }
+
+        private void tmrTimer_Tick(object sender, EventArgs e)
+        {
+            gGraph.Clear(Color.Black);
+
+            if (logButtons)
+            {
+                int down = imgGraph.Height / 4;
+                int up = 3 * imgGraph.Height / 4;
+
+                Pen pen = Pens.White;
+                Point point1 = new Point(0, up);
+
+                foreach (var d in data)
+                {
+                    try
+                    {
+                        double timeRatio = d.Key / (DateTime.Now - startTime).TotalSeconds;
+
+                        Point point2 = new Point((int)(timeRatio * imgGraph.Width), d.Value % 2 == 0 ? down : up);
+
+                        gGraph.DrawLine(pen, point1, new Point(point2.X, point1.Y));
+
+                        switch (d.Value)
+                        {
+                            case 0:
+                                pen = Pens.Red;
+                                break;
+                            case 1:
+                                pen = Pens.White;
+                                break;
+                            case 2:
+                                pen = Pens.Green;
+                                break;
+                            case 3:
+                                pen = Pens.White;
+                                break;
+                            case 4:
+                                pen = Pens.Blue;
+                                break;
+                            case 5:
+                                pen = Pens.White;
+                                break;
+                            default:
+                                pen = Pens.Gray;
+                                break;
+                        }
+
+                        gGraph.DrawLine(pen, new Point(point2.X, point1.Y), point2);
+
+                        point1.X = point2.X;
+                        point1.Y = point2.Y;
+                    }
+                    catch (DivideByZeroException)
+                    { }
+                }
+
+                gGraph.DrawLine(pen, point1, new Point(imgGraph.Width, point1.Y));
+            }
+            else if (!logButtons)
+            {
+                int midPoint = imgGraph.Height / 2;
+
+                Pen pen = Pens.White;
+                Point point1 = new Point(0, midPoint);
+
+                foreach (var d in data)
+                {
+                    try
+                    {
+                        double timeRatio = d.Key / (DateTime.Now - startTime).TotalSeconds;
+
+                        Point point2 = new Point((int)(timeRatio * imgGraph.Width),
+                                                 midPoint + ((3 * imgGraph.Height / 4) / (maxY - minY)) * -d.Value);
+
+                        gGraph.DrawLine(pen, point1, new Point(point2.X, point1.Y));
+
+                        if (d.Value > 0)
+                        {
+                                pen = Pens.Green;
+                        }
+                        else if (d.Value < 0)
+                        {
+                                pen = Pens.Red;
+                        }
+
+                        gGraph.DrawLine(pen, new Point(point2.X, point1.Y), point2);
+
+                        point1.X = point2.X;
+                        point1.Y = point2.Y;
+                    }
+                    catch (DivideByZeroException)
+                    { }
+                }
+
+                gGraph.DrawLine(pen, point1, new Point(imgGraph.Width, point1.Y));
+            }
+
+            imgGraph.Invalidate();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (running)
+            {
+                StopRecording();
+            }
+
+            if (data.Count > 0)
+            {
+                dlgSave.Filter = "Comma-Separated Values|*.csv";
+                dlgSave.AddExtension = true;
+                dlgSave.DefaultExt = ".csv";
+
+                DialogResult result = dlgSave.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    StreamWriter output = new StreamWriter(dlgSave.FileName);
+
+                    output.WriteLine("Time (s),Value");
+                    output.WriteLine("0,Start");
+                    foreach (var d in data)
+                    {
+                        if (logButtons)
+                        {
+                            output.WriteLine(d.Key + "," + buttons[d.Value]);
+                        }
+                        else
+                        {
+                            output.WriteLine(d.Key + "," + d.Value);
+                        }
+                    }
+                    output.Close();
+                }
+            }
+        }
+
+        private void rdoButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            logButtons = rdoButtons.Checked;
+        }
+
+        private void rdoYAxis_CheckedChanged(object sender, EventArgs e)
+        {
+            logButtons = !rdoYAxis.Checked;
+        }
+
+        private void frmMouseLab_Move(object sender, EventArgs e)
+        {
+            loggingPosition = new Point(Left + imgGraph.Left + imgGraph.Width / 2,
+                                        Top + imgGraph.Top + imgGraph.Height);
+        }
+    }
+}
